9f9c29cd94bc8f866fb86471da768cdfcd90fa27
Đặng Anh Tú <tuda@talent.ominext.com>
2020-04-11 10:18:53 +0700

    delete severTLU

--- ServerTLU/src/main/java/springboot/server/model/Student.java
+++
@@ -1,4 +0,0 @@
-package springboot.server.model;
-
-public class Student {
-}
--- ServerTLU/src/main/java/springboot/server/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package springboot.server.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import springboot.server.model.Category;
-
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-}
